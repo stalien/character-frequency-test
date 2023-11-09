3474a9a6ff4213adde66895744ef7742ba31e1f6
stalien <stalien@yandex.ru>
2023-11-09 16:49:09 +0300

    first code implementation

---
+++ src/main/java/com/example/characterfrequencytest/controllers/CharacterFrequencyController.java
@@ -0,0 +1,27 @@
+package com.example.characterfrequencytest.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api")
+public class CharacterFrequencyController {
+
+    @PostMapping("/frequency")
+    public List<Map.Entry<Character, Integer>> calculateFrequency(@RequestBody String input) {
+
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+        for (char c : input.toCharArray()) {
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+        }
+
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(frequencyMap.entrySet());
+
+        return list;
+    }
+
+}
