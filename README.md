646b275544ea22f5a84309fecbe5a743d3c43310
stalien <stalien@yandex.ru>
2023-11-09 16:05:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# character-frequency-test
---
+++ src/main/java/com/example/characterfrequencytest/CharacterFrequencyTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.characterfrequencytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterFrequencyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterFrequencyTestApplication.class, args);
+	}
+
+}
